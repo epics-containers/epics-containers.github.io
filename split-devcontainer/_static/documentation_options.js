b26f015b69710b24110c94e03b658a60e308128f
gilesknap <gilesknap@users.noreply.github.com>
2023-04-19 08:52:54 +0000

    deploy: ef36ddd190c74b0d63ee27cf52d34ee19957c1c9

--- split-devcontainer/_static/documentation_options.js
+++ split-devcontainer/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.10.dev7+g11daa4b',
+    VERSION: '0.10.dev10+gef36ddd',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
