34ff660e2236bae0f5d7840dc770b8d6f6f99db0
callumforrester <callumforrester@users.noreply.github.com>
2023-04-19 08:04:02 +0000

    deploy: 11daa4b9d85f2e651b9f0f9d841f07127eb8afa6

--- split-devcontainer/_static/documentation_options.js
+++ split-devcontainer/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.10.dev6+gc7af5c9',
+    VERSION: '0.10.dev7+g11daa4b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
