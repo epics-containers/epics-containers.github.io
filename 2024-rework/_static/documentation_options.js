5d753fdad836dbabe1193ffd6cf1576a19e8099b
gilesknap <gilesknap@users.noreply.github.com>
2024-01-23 09:43:33 +0000

    deploy: 58d643e65e85be0b9fa6a0e88f4736215c624275

--- 2024-rework/_static/documentation_options.js
+++ 2024-rework/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '2023.11.2.dev39+gb85982b',
+    VERSION: '2023.11.2.dev41+g58d643e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
